521e731a7f7046d7d5a33505b6ee9bc8a9520ff4
Abhishek Yadav <abhishek.yadav57@gmail.com>
2019-01-15 19:34:10 +0530

    modification

--- ansible/regtest/calculate_failed.sh
+++ ansible/regtest/calculate_failed.sh
@@ -3,8 +3,8 @@
 failed_no=$(grep -c "Failed" $1)
 passed_no=$(grep -c "Passed" $1)
 
-echo Failed = $failed_no
-echo Passed = $passed_no
+#echo Failed = $failed_no
+#echo Passed = $passed_no
 
 total=$((failed_no + passed_no))
 final_count=$failed_no'/'$total
